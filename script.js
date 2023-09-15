a2f527f698e2ca4c44d8d7d3c132ffc7e10574b6
Shihab <hello@shihabiiuc.com>
2023-09-15 23:21:29 +0600

    small adjustments

--- script.js
+++ script.js
@@ -3,16 +3,13 @@ var textarea = document.getElementById('textarea');
 var clicks = document.getElementById('count');
 var dcCount = document.getElementById('dcCount');
 var reset = document.getElementById('reset');
-
 reset.onclick = function () {
     clicks.value = 0;
     dcCount.value = 0;
     textarea.value = '';
     click.style.background = '#182860';
 }
-
 var prevClickTime = Date.now();
-
 function clickEvent() {
     var currentTime = Date.now();
     var diff = currentTime - prevClickTime;
@@ -21,10 +18,8 @@ function clickEvent() {
         click.style.background = 'red';
         dcCount.value++;
     }
-
     textarea.value = diff + " ms\n" + textarea.value;
     prevClickTime = currentTime;
     clicks.value++;
 }
-
 click.addEventListener('click', clickEvent);
